b11cb0f7d12e7a078f0d43928774458ebe9334e9
Sergey Volkov <f.focus1377@gmail.com>
2023-03-27 22:53:17 +0300

    main

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <ItemTimer />
+  </div>
+</template>
+
+<script setup>
+import ItemTimer from "./components/ItemTimer.vue";
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  background: #353638;
+  height: 100vh;
+  margin: 0 auto;
+}
+</style>
